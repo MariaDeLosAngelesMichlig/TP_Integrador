bc08cbee36faa5f46b5683794a261b5fdb4a3045
MariaDeLosAngelesMichlig <153042361+MariaDeLosAngelesMichlig@users.noreply.github.com>
2023-12-06 01:26:22 -0300

    TP_Completo
    Profesor, se suben todos los archivos del proyecto: Modelado del DER/UML, Base de Datos y Proyecto en NetBeans. Muchas gracias.

---
+++ BD_Torneo.sql
@@ -0,0 +1,11 @@
+use torneo;
+
+INSERT INTO emun_resultado (Valores)
+VALUES ("VICTORIA");
+
+INSERT INTO emun_resultado (Valores)
+VALUES ("EMPATE");
+
+INSERT INTO emun_resultado (Valores)
+VALUES ("DERROTA");
+ 
---
+++ ENUM_RESULTADO.sql
@@ -0,0 +1,1 @@
+SELECT * FROM torneo.emun_resultado;
